565cd5af32c08684c9813725ff2bee689c9a7e61
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2025-03-14 19:57:44 -0300

    Update ExercíciosFase1.java

--- TRABALHO1/ExercíciosFase1.java
+++ TRABALHO1/ExercíciosFase1.java
@@ -133,6 +133,113 @@ import java.util.Scanner;
 //6. Cálculo do Salário
 //Escreva um programa que receba o salário bruto de um funcionário e o valor de desconto do INSS. O programa deve calcular e exibir o salário líquido.
 
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double salario;
+		double inss;
+		double salarioBruto;
+		
+		System.out.println("Digite o valor do salario: ");
+		salario = sc.nextDouble();
+		
+		System.out.println("Digite o valor do INSS: ");
+		inss = sc.nextDouble();
+		
+		salarioBruto = salario - inss;
+		
+		System.out.println("O valor do salario total: "+ salarioBruto);
+
+		
+		sc.close();
+	}
+
+//7. Cálculo do Perímetro e Área de um Retângulo
+//Faça um programa que solicite a base e a altura de um retângulo e exiba o seu perímetro e sua área.
+
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double base;
+		double altura;
+		double area;
+		double perimetro;
+		
+		System.out.println("Digite o valor da base: ");
+		base = sc.nextDouble();
+		
+		System.out.println("Digite o valor da altura: ");
+		altura = sc.nextDouble();
+		
+		area = base * altura;
+		perimetro = 2 * (base * altura);
+		
+		System.out.println("O valor da área é: "+ area);
+		System.out.println("O valor do perimetro é: "+ perimetro);
+		
+		sc.close();
+	}
+
+//8. Cálculo do Volume de uma Esfera
+//Crie um programa que leia o raio de uma esfera e calcule o seu volume.
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double raio;
+		double volume;
+		
+		System.out.println("Digite o valor do raio");
+		raio = sc.nextDouble();
+		
+		volume = (4/3)*3.14159*(raio*raio*raio);
+		
+		System.out.println("O valor do Volume é: "+volume);
+		
+		sc.close();
+	}
+
+//9. Conversão de Moeda
+//Faça um programa que leia um valor em reais (R$) e a cotação do dólar no dia. O programa deve calcular e exibir o valor equivalente em dólares (US$).
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double real;
+		double dolar;
+		double conversao;
+		
+		System.out.println("Digite o valor em reais: ");
+		real = sc.nextDouble();
+		
+		System.out.println("Digite o valor da cotação do dolar: ");
+		dolar = sc.nextDouble();
+		
+		conversao = real * dolar;
+		
+		System.out.println("O valor da conversao é: "+ conversao);
+			
+		sc.close();
+	}
+
+//10. Distância Entre Dois Pontos
+//Desenvolva um programa que leia as coordenadas x1, y1 e x2, y2 de dois pontos no plano cartesiano e calcule a distância entre eles.
+
+
+
 
 
 
