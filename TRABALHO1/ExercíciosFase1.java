90341ea5be5e61c2d62cf792b3a45cc09ecfdece
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2025-03-14 17:24:27 -0300

    Create ExercíciosFase1.java

---
+++ TRABALHO1/ExercíciosFase1.java
@@ -0,0 +1,38 @@
+//exercício 01
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		package exercicio_fase1;
+import java.util.Scanner;
+
+public class exercicio01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1;
+		int num2;
+		int soma;
+		
+		soma = num1 + num2;
+		
+		System.out.println("Digite o primeiro numero: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		num2 = sc.nextInt();
+		
+		sc.close();
+	}
+ 
+
+	}
+
+}
+
+//exercício 02
+  
