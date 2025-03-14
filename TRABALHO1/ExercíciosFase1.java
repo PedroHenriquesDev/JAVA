5f640eec99a9e9b746d10d3b43c6983edc9f2ca4
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2025-03-14 18:56:37 -0300

    Update ExercíciosFase1.java

--- TRABALHO1/ExercíciosFase1.java
+++ TRABALHO1/ExercíciosFase1.java
@@ -28,8 +28,6 @@ import java.util.Scanner;
 
 import java.util.Scanner;
 
-public class exercicio01 {
-
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -45,11 +43,103 @@ public class exercicio01 {
 		System.out.println("O valor da area é: "+ area);
 		
 		
+		sc.close();
+	}
+
+
+
+//3. Conversão de Temperatura
+//Crie um programa que leia uma temperatura em graus Celsius e a converta para Fahrenheit.
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int celsius;
+		int Fahrenheit;
+		
+		System.out.println("Digite o valor da temperatura em graus celsius");
+		celsius = sc.nextInt();
+		
+		Fahrenheit = (celsius * 9/5) + 32;
+		
+		System.out.println("O valor em Fahrenheit é: " + Fahrenheit);
+		
 		
 		sc.close();
 	}
 
 }
+
+//4. Cálculo de Média Simples
+//Faça um programa que solicite ao usuário três notas (valores decimais) e exiba a média aritmética delas.
+
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double nota1;
+		double nota2;
+		double nota3;
+		double media;
+		
+		System.out.println("Digite a primeira nota: ");
+		nota1 = sc.nextDouble();
+		
+		System.out.println("Digite a segunda nota: ");
+		nota2 = sc.nextDouble();
+		
+		System.out.println("Digite a terceira nota: ");
+		nota3 = sc.nextDouble();
+		
+		media = (nota1 + nota2 + nota3)/3;
+		
+		System.out.println("O valor da média é: " + media);
+
+		
+		sc.close();
+	}
+
+//5. Conversão de Idade para Dias
+//Desenvolva um programa que leia a idade de uma pessoa em anos e exiba a quantidade aproximada de dias que ela já viveu.
+
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int idade;
+		int dias;
+		
+		System.out.println("Digite sua idade: ");
+		idade = sc.nextInt();
+		
+		dias = idade * 365;
+		
+		System.out.println("A quantidade de dias aproximadamente é de: "+ dias);
+		
+		
+		
+		sc.close();
+	}
+
+//6. Cálculo do Salário
+//Escreva um programa que receba o salário bruto de um funcionário e o valor de desconto do INSS. O programa deve calcular e exibir o salário líquido.
+
+
+
+
+
+
+
+
 
 
   
