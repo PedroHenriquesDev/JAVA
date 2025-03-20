63c8f85d52351a4e3e8eef2af0ce28f27d299e1b
PedroHenriquesDev <133235396+PedroHenriquesDev@users.noreply.github.com>
2025-03-20 19:44:16 -0300

    Update and rename ExercíciosFase1.java to EX01.java

---
+++ TRABALHO1/EX01.java
@@ -0,0 +1,39 @@
+//1. Soma de Dois Números
+//Escreva um programa que solicite ao usuário dois números inteiros e exiba a soma deles.
+
+import java.util.Scanner;
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num1;
+		int num2;
+		int soma;
+		
+		soma = num1 + num2;
+		
+		System.out.println("Digite o primeiro numero: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Digite o segundo numero: ");
+		num2 = sc.nextInt();
+		
+		sc.close();
+	}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
